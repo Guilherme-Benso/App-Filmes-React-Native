bf63a3e84f484fcb178aeb445ae1398acab1838b
Guilherme <guilhermesouzabenso@gmail.com>
2023-01-02 14:55:41 -0300

    New: novas linhas adicionadas

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { 
+    View,
+    Text,
+    ScrollView,
+    StyleSheet,
+    Dimensions,
+    ImageBackground
+
+} from 'react-native';
+
+//Criando duas constantes
+const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
 
 export default function Home() {
  return (
-   <View>
-    
-   </View>
+   <ScrollView style={styles.container}>
+        <View style={{flex:1, height:screenHeight}}>
+            <View style={{...StyleSheet.absoluteFill, backgroundColor: '#000'}}>
+
+            </View>
+        </View>
+   </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
